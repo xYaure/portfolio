f8a8823eaac6d11ac95e54975f082b358452d8fd
xYaure <sebastianklak6@gmail.com>
2023-10-10 18:21:01 +0200

    Some chnges

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './services.css'
+import Image1 from "../../assets/service-1.svg"
+import Image2 from "../../assets/service-2.svg"
+import Image3 from "../../assets/service-3.svg"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="services container" id='services'>
+      <h2>Co oferuje?</h2>
+      <div className="services__container section grid">
+        <div className="services__data">
+          <div className="services__img">
+            <img src={Image1} alt="" />
+          </div>
+          <div className="services__description">
+            <p>Stworzę przyjazną dla oka, profesjonalną, nowoczesną oraz responsywną stronę internetową ze wszystkimi upodobaniami i wymaganiami jakie klient sobie zażyczy.</p>
+          </div>
+        </div>
+        <div className="services__data">
+          <div className="services__img">
+            <img src={Image2} alt="" />
+          </div>
+          <div className="services__description">
+            <p>Tworzę również aplikacje mobilne/desktopowe w języku C#, nie mam wysokiego doświadczenia lecz można liczyć na średnio-zaawansowane aplikacje</p>
+          </div>
+        </div>
+        <div className="services__data">
+          <div className="services__img">
+            <img src={Image3} alt="" />
+          </div>
+          <div className="services__description">
+            <p>Umiem edytować zdjęcia, poprawiać jakość, tworzyć ikony/loga na strony internetowe oraz dla aplikacji w Adobe Illustrator i Adobe Photoshop</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
