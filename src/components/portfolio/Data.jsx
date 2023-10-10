c2e201c9709a8d54c3ecaba1bafac364e9eb7f01
xYaure <sebastianklak6@gmail.com>
2023-10-10 18:50:56 +0200

    Portfolio

---
+++ src/components/portfolio/Data.jsx
@@ -0,0 +1,28 @@
+const Data = [
+    {
+        id: 1,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2019 - teraz",
+        title: "Technikum Informatyczne",
+        desc: "Szkoła średnia z wykształceniem informatycznym (INF.02, INF.03)"
+    },
+    {
+        id: 2,
+        category: "experiance",
+        icon: "icon-briefcase",
+        year: "2023",
+        title: "Praktyki",
+        desc: "Miesięczne szkolne praktyki w firmie Fakro na dziale IT"
+    },
+    {
+        id: 3,
+        category: "experiance",
+        icon: "icon-briefcase",
+        year: "2021",
+        title: "Praktyki",
+        desc: "Miesięczne szkolne praktyki w firmie Telebeskid na dziale IT"
+    }
+]
+
+export default Data
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './portfolio.css'
+import Data from './Data'
+import Card from './Card'
 
 const Portfolio = () => {
   return (
     <section className="portfolio container section" id='portfolio'>
       <h2 className="section__title">Doświadczenie</h2>
+
+      <div className="portfolio__container section grid">
+        <div className="timeline grid">
+          {Data.map((val, id) => {
+            if (val.category === "education") {
+              return (
+                <Card key={id} icon={val.icon} title={val.title} year={val.year} desc={val.desc} />
+              )
+            }
+          })}
+        </div>
+
+        <div className="timeline grid">
+          {Data.map((val, index) => {
+            if (val.category === "experiance") {
+              return (
+                <Card key={index} icon={val.icon} title={val.title} year={val.year} desc={val.desc} />
+              )
+            }
+          })}
+        </div>
+      </div>
     </section>
   )
 }
