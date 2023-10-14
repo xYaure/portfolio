b9c5e9c747f4f2b174322e005e360a1ba6f04e2f
xYaure <sebastianklak6@gmail.com>
2023-10-14 09:57:00 +0200

    Projects

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,5 +1,6 @@
 import Image1 from './img/first-site.png'
-import Image2 from './img/second-site.png'
+import Image2 from './img/first-code.png'
+import Image3 from './img/second-site.png'
 
 
 const icons = ["bi bi-browser-chrome project__icon", "bi bi-code-slash project__icon"]
@@ -9,21 +10,24 @@ const Data = [
         image: Image1,
         name: "Strona internetowa Portfolio",
         category: "website",
-        icon: icons[0]
+        icon: icons[0],
+        showCategory: 'website'
     },
     {
         id: 2,
         image: Image2,
         name: "Kod Data.jsx dla testu",
-        category: "website",
-        icon: icons[1]
+        category: "application",
+        icon: icons[1],
+        showCategory: 'application'
     },
     {
         id: 3,
-        image: Image1,
+        image: Image3,
         name: "Strona internetowa Portfolio wersja 1",
         category: "website",
-        icon: icons[0]
+        icon: icons[0],
+        showCategory: 'website'
     }
 ]
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './projects.css'
 import Project from './Project'
 import Data from './Data'
 
 
 const Projects = () => {
+    const [category, setCategory] = useState(true);
   return (
     <section id='projects' className='projects section '>
         <div className='project__container'>
             <div className='project__tittle'>
                 <h2 className="section__tittle">Projekty</h2>
             </div>
+            <h3 className='project__categories'>Kategorie</h3>
+            <div className="project__buttons">
+                <button onClick={() => setCategory(true)} className='project__button'>Pokaż wszystkie </button>
+                <button onClick={() => setCategory('website')} className='project__button'>Strony internetowe </button>
+                <button onClick={() => setCategory('application')} className='project__button'>Aplikacje w języku C# </button>
+                <i onClick={() => setCategory(true)} class="bi small_device bi-folder"></i>
+                <i onClick={() => setCategory('website')} className='bi small_device bi-browser-chrome'></i>
+                <i onClick={() => setCategory('application')} className='bi small_device bi-code-slash'></i>
+            </div>
             <div className='project__list grid'>
                 {Data.map((val, id) => {
-                    return (
-                        <Project key={id} icon={val.icon} name={val.name} image={val.image} category={val.category}/>
-                    )
+                    if (category === true) {
+                        return (
+                            <Project key={id} icon={val.icon} name={val.name} image={val.image} category={category}/>
+                        )
+                    }
+                    else if (category === 'website' && category === val.showCategory) {
+                        return (
+                            <Project key={id} icon={val.icon} name={val.name} image={val.image} category={category}/>
+                        )
+                    }
+                    else if (category === 'application' && category === val.showCategory) {
+                        return (
+                            <Project key={id} icon={val.icon} name={val.name} image={val.image} category={category}/>
+                        )
+                    }
                 })}
             </div>
         </div>
